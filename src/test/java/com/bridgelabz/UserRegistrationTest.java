4c43f9297161dff67b9f8ae496d938a53c938b1c
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-04-05 16:53:02 +0530

    Added:Unit Test Validation for Multiple Entry

--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,77 +1,81 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.jupiter.api.*;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 public class UserRegistrationTest {
-    UserRegistration userRegistration = new UserRegistration();
-
+    UserRegistration userRegistration ;
+    @Before
+    public void initialize(){
+        userRegistration = new UserRegistration();
+    }
+    String emailId;
+    public UserRegistrationTest(String emailId){
+        super();
+        this.emailId = emailId;
+    }
     @Test
-    void givenFirstNameHappyTest() {
-        boolean actualValue = userRegistration.validateFirstName("Pallavi");
-        Assertions.assertTrue(actualValue);
-        System.out.println("FirstName Successfully Validate.");
+    public void firstNameHappyTest(){
+        boolean firstName = userRegistration.validateFirstName("Pallavi");
+        Assert.assertEquals(true,firstName);
+    }
+    @Test
+    public void firstNameSadTest(){
+        boolean firstName = userRegistration.validateFirstName("pallavis");
+        Assert.assertFalse(firstName);
+    }
+    @Test
+    public void LastNameHappyTest(){
+        boolean lastName = userRegistration.validateLastName("Kabadi");
+        Assert.assertTrue(lastName);
+    }
+    @Test
+    public void LastNameSadTest(){
+        boolean lastName = userRegistration.validateLastName("kabadii");
+        Assert.assertFalse(lastName);
+    }
+    @Test
+    public void phoneNumberHappyTest(){
+        boolean phoneNumber = userRegistration.validateMobileNumber("+91 8329192312");
+        Assert.assertTrue(phoneNumber);
+    }
+    @Test
+    public void phoneNumberSadTest(){
+        boolean phoneNumber = userRegistration.validateMobileNumber("918229192312");
+        Assert.assertFalse(phoneNumber);
+    }
+    @Test
+    public void passwordHappyTest(){
+        boolean password = userRegistration.validatePassword("pallavi@123");
+        Assert.assertTrue(password);
+    }
+    @Test
+    public void passwordSadTest(){
+        boolean password = userRegistration.validatePassword("pallavi");
+        Assert.assertFalse(password);
+    }
+    @Test
+    public void emailHappyTest(){
+        boolean email = userRegistration.validateEmail("pallavi08jadhav@gmail.com");
+        Assert.assertTrue(email);
+    }
+    @Test
+    public void emailSadTest(){
+        boolean email = userRegistration.validateEmail("Pallavi08Jadhav@gmail.com");
+        Assert.assertFalse(email);
     }
 
-    @Test
-    void givenFirstNameSadTest() {
-        boolean actualFirstName = userRegistration.validateFirstName("pallavis");
-        Assertions.assertFalse(actualFirstName);
-        System.out.println("FirstName Invalid.");
+    @Parameterized.Parameters
+    public static Collection input(){
+        return Arrays.asList( new String[] {"abc@yahoo.com","abc.100@abc.com.au","abc@1.com","abc+100@gmail.com","abc.100@yahoo.com","abc-100@abc.net","abc-100@yahoo.com","abc111@abc.com"});
     }
-
     @Test
-    void givenLastNameHappyTest() {
-        boolean actualFirstName = userRegistration.validateLastName("Kabadi");
-        Assertions.assertTrue(actualFirstName);
-        System.out.println("LastName Successfully Validate.");
-    }
-
-    @Test
-    void givenLastNameSadTest() {
-        boolean actualLastName = userRegistration.validateLastName("kabadii");
-        Assertions.assertFalse(actualLastName);
-        System.out.println("LastName Invalid.");
-    }
-
-    @Test
-    void givenEmailHappyTest() {
-        boolean actualEmail = userRegistration.validateEmail("pallavi08jadhav@gmail.com");
-        Assertions.assertTrue(actualEmail);
-        System.out.println("Email Successfully Validate.");
-    }
-
-    @Test
-    void givenEmailSadTest() {
-        boolean actualEmail = userRegistration.validateEmail("Pallavi08Jadhav@gmailcom");
-        Assertions.assertFalse(actualEmail);
-        System.out.println("Email Invalid.");
-    }
-
-    @Test
-    void givenMobileNumberHappyTest() {
-        boolean actualMobileNumber = userRegistration.validateMobileNumber("+91 8329192312");
-        Assertions.assertTrue(actualMobileNumber);
-        System.out.println("Mobile Number Successfully Validate.");
-    }
-
-    @Test
-    void givenMobileNumberSadTest() {
-        boolean actualEmail = userRegistration.validateMobileNumber("+918229192312");
-        Assertions.assertFalse(actualEmail);
-        System.out.println("Mobile Number Invalid.");
-    }
-
-    @Test
-    void givenPasswordHappyTest() {
-        boolean actualMobileNumber = userRegistration.validatePassword("PallaviJadhav@123");
-        Assertions.assertTrue(actualMobileNumber);
-        System.out.println("Password Successfully Validate.");
-    }
-
-    @Test
-    void givenPasswordSadTest() {
-        boolean actualPassword = userRegistration.validatePassword("PallaviJadhav");
-        Assertions.assertFalse(actualPassword);
-        System.out.println("Password Invalid.");
+    public void checkEmailWithMultipleInputs(){
+        Assert.assertEquals(true,userRegistration.validateEmail(emailId));
     }
 }
